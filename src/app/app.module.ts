799b652cae0737a0a87da2c982ca8dbb486d3f3f
jzbond <jzbond@gmail.com>
2017-12-01 15:13:56 +0100

    Main page components stubs

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,23 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './pages/common/header/header.component';
+import { FooterComponent } from './pages/common/footer/footer.component';
+import { LogoComponent } from './pages/common/logo/logo.component';
+import { LoginComponent } from './pages/login/login.component';
+import { CoursesComponent } from './pages/courses/courses.component';
+import { CourseComponent } from './pages/courses/course/course.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    LogoComponent,
+    LoginComponent,
+    CoursesComponent,
+    CourseComponent
   ],
   imports: [
     BrowserModule
