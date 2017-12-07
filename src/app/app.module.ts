f43933d4f1a3f8a8dde95b028247948460c33c94
jzbond <jzbond@gmail.com>
2017-12-07 21:02:16 +0100

    Implement header and footer components and use them in App component.
    This is just stub implementation to layout main page.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,15 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
 
-import { AppComponent } from './app.component';
-import { HeaderComponent } from './pages/common/header/header.component';
-import { FooterComponent } from './pages/common/footer/footer.component';
-import { LogoComponent } from './pages/common/logo/logo.component';
-import { LoginComponent } from './pages/login/login.component';
-import { CoursesComponent } from './pages/courses/courses.component';
-import { CourseComponent } from './pages/courses/course/course.component';
-
+import {AppComponent} from './app.component';
+import {HeaderComponent} from './pages/common/header/header.component';
+import {FooterComponent} from './pages/common/footer/footer.component';
+import {LogoComponent} from './pages/common/logo/logo.component';
+import {LoginComponent} from './pages/login/login.component';
+import {CoursesComponent} from './pages/courses/courses.component';
+import {CourseComponent} from './pages/courses/course/course.component';
+import {MatToolbarModule} from '@angular/material';
 
 @NgModule({
   declarations: [
@@ -19,12 +19,14 @@ import { CourseComponent } from './pages/courses/course/course.component';
     LogoComponent,
     LoginComponent,
     CoursesComponent,
-    CourseComponent
+    CourseComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MatToolbarModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
