8522fa5e1f8ff00a24e2917978884b1e1b04d96d
jzbond <jzbond@gmail.com>
2017-12-16 01:03:42 +0100

    Fix Courses component tests dependencies.

--- src/app/pages/courses/courses.component.spec.ts
+++ src/app/pages/courses/courses.component.spec.ts
@@ -4,6 +4,7 @@ import {CoursesComponent} from './courses.component';
 import {CourseComponent} from './course/course.component';
 import {ToolbarComponent} from './toolbar/toolbar.component';
 import {MatCardModule} from "@angular/material";
+import {MatDialogModule} from "@angular/material/dialog";
 import {FormsModule} from "@angular/forms";
 
 describe('CoursesComponent', () => {
@@ -12,7 +13,7 @@ describe('CoursesComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      imports: [MatCardModule, FormsModule],
+      imports: [MatCardModule, MatDialogModule, FormsModule],
       declarations: [CoursesComponent, CourseComponent, ToolbarComponent]
     })
       .compileComponents();
